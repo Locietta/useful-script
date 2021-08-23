24ea430941ea42ff18a62f7b3cb5d16c447042d1
Locietta <locietta@qq.com>
2021-08-23 10:24:24 +0800

    a wrap for using MSYS2 git in VSCode

---
+++ git-wrap.bat
@@ -0,0 +1,13 @@
+rem https://github.com/microsoft/vscode/issues/4651
+
+@echo off
+setlocal
+
+rem If you don't add path for msys2 into %PATH%, enable following line.
+rem set PATH=c:\msys64\usr\bin;%PATH%
+
+if "%1" equ "rev-parse" goto rev_parse
+git %*
+goto :eof
+:rev_parse
+for /f %%1 in ('git %*') do cygpath -w %%1
