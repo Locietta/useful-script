4c4ee893fe7baec028d4f5e576737136debdd44c
Locietta <locietta@qq.com>
2021-09-24 01:48:14 +0800

    for X410 option

--- Connect-ArchVM.ps1
+++ Connect-ArchVM.ps1
@@ -1,12 +1,15 @@
 $VMRUN = "D:\\VMWare\vmrun.exe"
 $X_SRV = "D:\\Scoop\\apps\\vcxsrv\\current\\vcxsrv.exe"
+# $X_SRV = "C:\\Users\pc\AppData\Local\Microsoft\WindowsApps\x410.exe"
 $Machine = "D:\\.WSL\\ArchLinuxVMWare\\ArchLinux.vmx"
 
 $env:DISPLAY = "localhost:0.0" # set X recieve port
 
 try { # start X server if not started
-    $SILENT = Get-Process "vcxsrv" -ErrorAction Stop; # to make it silent
+    # $SILENT = Get-Process "x410" -ErrorAction Stop; # to make it silent
+    $SILENT = Get-Process "vcxsrv" -ErrorAction Stop; 
 } catch { # start X server before vm, or X Forwarding will fail
+    # &$X_SRV ":0" /wm /public;
     &$X_SRV ":0" -clipboard -multiwindow -ac -wgl;
 } 
 
@@ -15,4 +18,4 @@ if (-not $FIND_MACHINE) { # start VM if not started
     &$VMRUN -T ws start $Machine nogui;
 }
 
-ssh.exe -Y loia@192.168.159.128;
+ssh.exe -Y loia@192.168.159.129;
