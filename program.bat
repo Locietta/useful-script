25a83beb4fbe286defdff9ca3f3b11928f876d21
Locietta <locietta@qq.com>
2021-11-01 17:24:09 +0800

    wrapper for ise impact programmer

---
+++ program.bat
@@ -0,0 +1,15 @@
+@echo off
+(
+echo setMode -bs
+echo setCable -port auto
+echo Identify -inferir
+echo identifyMPM
+echo assignFile -p 1 -file %1
+echo Program -p 1
+) > D:\.config\Vivado-shortcut\temp_cmd.cmd
+
+D:\Xilinx\14.6\LabTools\LabTools\bin\nt\impact.exe -batch D:\.config\Vivado-shortcut\temp_cmd.cmd
+
+@REM del D:\.config\Vivado-shortcut\temp_cmd.cmd
+
+del %~dp0/_impactbatch.log
