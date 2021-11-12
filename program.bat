5f76ae93cc58889dc914b81cc29a8099e4780795
Locietta <locietta@qq.com>
2021-11-12 21:30:44 +0800

    mv temp script to cache

--- program.bat
+++ program.bat
@@ -8,8 +8,8 @@ echo assignFile -p 1 -file %1
 echo Program -p 1
 ) > D:\.config\Vivado-shortcut\temp_cmd.cmd
 
-D:\Xilinx\14.6\LabTools\LabTools\bin\nt\impact.exe -batch D:\.config\Vivado-shortcut\temp_cmd.cmd
+D:\Xilinx\14.6\LabTools\LabTools\bin\nt\impact.exe -batch D:\.config\Vivado-shortcut\cache\temp_cmd.cmd
 
-@REM del D:\.config\Vivado-shortcut\temp_cmd.cmd
+@REM del D:\.config\Vivado-shortcut\cache\temp_cmd.cmd
 
 del %cd%\_impactbatch.log
