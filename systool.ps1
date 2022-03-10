f58098938328dee6e2022380687859f816b95c01
Locietta <locietta@qq.com>
2022-03-10 09:54:17 +0800

    add new utility for systool
    * change the port of remote desktop
    * toggle powershell locale between utf-8 and gbk

--- systool.ps1
+++ systool.ps1
@@ -42,6 +42,38 @@ $commandList = @{
             }
         }
     )
+    "set_rdp" = @(
+        @{
+            "port" = $null
+        },
+        {
+            if ($stuff -match "^([1-9]\d*|0)$") {
+                Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber" -Value $stuff 
+                New-NetFirewallRule -DisplayName 'RDPPORTLatest-TCP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $stuff 
+                New-NetFirewallRule -DisplayName 'RDPPORTLatest-UDP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol UDP -LocalPort $stuff
+            } else {
+                Write-Output "Not a valid port number: ``$stuff``"
+            }
+        }
+    )
+    "locale" = @(
+        @{
+            "utf-8" = {
+                [System.Console]::OutputEncoding=[System.Text.Encoding]::GetEncoding(65001)
+            }
+            "gbk" = {
+                [System.Console]::OutputEncoding=[System.Text.Encoding]::GetEncoding(936)
+            }
+        },
+        {
+            param([hashtable] $locale_objs = $commandList.locale[0])
+            if ($locale_objs.ContainsKey($stuff)) {
+                &$locale_objs.$stuff
+            } else {
+                Write-Output "Can't change to unsupported locale: ``$stuff``"
+            }
+        }
+    )
 }
 
 if ($help -or !$subCommand) {
