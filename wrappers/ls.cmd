e455a43f318023c9e676bb1d657d7d003e6f99d9
Locietta <locietta@qq.com>
2022-06-30 15:26:36 +0800

    wrappers: simplify `ls`

--- wrappers/ls.cmd
+++ wrappers/ls.cmd
@@ -2,7 +2,6 @@
 for /f "tokens=1,* delims= " %%a in ("%*") do set ALL_BUT_1=%%b
 if "%1"=="" goto noarg
 wsl -d Ubuntu -- ls --color=tty $(wslpath %1) %ALL_BUT_1%
-goto end
+goto :eof
 :noarg
 wsl -d Ubuntu -- ls --color=tty
-:end
