2080a904e84373522906179f3445df4abf9f4526
Locietta <locietta@qq.com>
2022-12-16 00:54:41 +0800

    change hard coded path to `%MSYS2%`

--- wrappers/ucrt64.cmd
+++ wrappers/ucrt64.cmd
@@ -1,6 +1,6 @@
 @echo off
 if "%1"=="" goto noarg
-D:/msys64/msys2_shell.cmd -defterm -no-start -here -ucrt64 -c "%*"
+%MSYS2%\msys2_shell.cmd -defterm -no-start -here -ucrt64 -c "%*"
 goto :eof
 :noarg
-D:/msys64/msys2_shell.cmd -defterm -no-start -here -ucrt64
+%MSYS2%\msys2_shell.cmd -defterm -no-start -here -ucrt64
