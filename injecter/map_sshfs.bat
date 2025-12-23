6ac2a59580d9b06f001a2aa1c7cdd58d54c3a4b7
Locietta <locietta@gmail.com>
2025-12-23 14:45:19 +0800

    sshfs: migrate server from Osaka to Tokyo

--- injecter/map_sshfs.bat
+++ injecter/map_sshfs.bat
@@ -1,3 +1,3 @@
-start /B "loia@Osaka" "%ProgramFiles%\SSHFS-Win\bin\sshfs-win.exe" svc \sshfs.k\loia@Osaka X: "-o ServerAliveInterval=30"
-start /B "root@Osaka" "%ProgramFiles%\SSHFS-Win\bin\sshfs-win.exe" svc \sshfs.kr\root@Osaka Y: "-o ServerAliveInterval=30"
+start /B "loia@TokyoYY" "%ProgramFiles%\SSHFS-Win\bin\sshfs-win.exe" svc \sshfs.k\loia@TokyoYY!16217 X: "-o ServerAliveInterval=30"
+start /B "root@TokyoYY" "%ProgramFiles%\SSHFS-Win\bin\sshfs-win.exe" svc \sshfs.kr\loia@TokyoYY!16217 Y: "-o ServerAliveInterval=30"
 start /B "shadiao@shadiao" "%ProgramFiles%\SSHFS-Win\bin\sshfs-win.exe" svc \sshfs.k\shadiao@shadiao Z: "-o ServerAliveInterval=30"
